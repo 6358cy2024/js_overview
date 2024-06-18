18be8adbd7b237a6e5b0e4b1a4526758ac26c0c5
Chris Yanez <cy2019@hotmail.com>
2024-06-18 12:42:10 -0400

    finished variables and data types and array

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 console.log(typeof 3.2);
+var age = 44;
+var isADev = true;
+var n = null;
+var notANumber = NaN;
 // This shows up in the Inspect Console on the webpage.
 console.log('Christian Yanez')
 console.log(5 + 5);
-var firstName;
+var firstName = "Christian";
+var lastName = "Yanez";
 console.log(firstName);
+var nothing;
+var another;
+var fullName = firstName + ' ' + lastName;
+var names = ['John', 'Ann', 'Bob', 'Frank', 'Jane'];
+console.log(names.length);
+var ages = [30, 24, 55, 99];
+console.log(ages);
+console.log(names);
+
+names.splice(3, 2);
+console.log(names);
