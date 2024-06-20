681ab7785097dc8be32d6713c4832d0b44741d21
Chris Yanez <cy2019@hotmail.com>
2024-06-20 13:36:15 -0400

    Finished advanced variables

--- js/script.js
+++ js/script.js
@@ -1,23 +1,17 @@
 
-var firstName = "Christian";
-var lastName = "Yanez";
-var age = 44;
-var names = ['John', 'Ann', 'Bob', 'Frank', 'Jane'];
-console.log(age > 30); 
-console.log(age === '44');
-console.log(0 || null || '' || -5 || undefined); //truthy or falsey
-var oldEnough = (age >= 21);
+// var num = 10;
+// console.log(num);
+//     if(true){
+//         var studentName = 'Christian';
+//     }
+// console.log(studentName);
 
-//conditionals
-if (firstName && lastName) {
-    console.log("Fullname");
-}
-else if (!firstName) {
-    console.log("please privde your first name!");
-}
-else if (!lastName) {
-    console.log("please privde your last name!");
-}
-else {
-    console.log("please provide your full name");
-}
+// let num = 10;
+// console.log(num);
+//let is basically var, only to be used within functions, like if
+
+// CONST
+const num = 10;
+const people = ['Bob', 'John'];
+people.pop();
+console.log(people);
