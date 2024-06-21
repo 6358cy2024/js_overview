d4b7df5bb7857c644dbc14675bbdbb2942c195fa
Chris Yanez <cy2019@hotmail.com>
2024-06-21 12:39:42 -0400

    finished calculator

--- js/script.js
+++ js/script.js
@@ -1,31 +1,29 @@
+/*make a function that takes three arguments
+number, number, operater, 
+*/
+const body = document.body;
+body.style.backgroundColor = 'red';
 
-// function myFirstFunc() {
-//     console.log('function has been called');
-// }
-
-// myFirstFunc();
-
-// function add(num1, num2) {
-//     console.log(num1 + num2);
-// }
-// add(5, 10);
-
-// function printValues(num, boolean, name, another){
-//     console.log(boonlean);
-// }
-// printValues(10, 'Allen', false, 'another');
-
-function printNames(names){
-    for(const name of names){
-        console.log(name);
+function calc(num1, num2, operator){
+    let result;
+    if (operator == '+'){
+        result = (num1 + num2);
+        console.log(result);
+    }
+    else if (operator == '-'){
+        result = (num1 - num2);
+        console.log(result);
+    }
+    else if (operator == '*'){
+        result = (num1 * num2);
+        console.log(result);
+    }
+    else if (operator == '/'){
+        result = (num1 / num2);
+        console.log(result);
+    }
+    else{
+        console.log("Invalid"); //for if operator is invalid
     }
 }
-printNames(['Bob', 'Jimmy', 'Julie'])
-
-const someStr = 'some string';
-console.log(someStr[6]);
-
-// const someName = 'Christian';
-// const h1 = document.querySelector('h1');
-
-h1.innerText =  someName;
+calc(4, 9, '-');//needs quotations
