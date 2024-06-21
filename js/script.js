314e36cc99d2a68e34ce0d0ff695e40ad494bf42
Chris Yanez <cy2019@hotmail.com>
2024-06-21 10:19:06 -0400

    Finished function basics

--- js/script.js
+++ js/script.js
@@ -1,17 +1,31 @@
 
-// var num = 10;
-// console.log(num);
-//     if(true){
-//         var studentName = 'Christian';
-//     }
-// console.log(studentName);
+// function myFirstFunc() {
+//     console.log('function has been called');
+// }
 
-// let num = 10;
-// console.log(num);
-//let is basically var, only to be used within functions, like if
+// myFirstFunc();
 
-// CONST
-const num = 10;
-const people = ['Bob', 'John'];
-people.pop();
-console.log(people);
+// function add(num1, num2) {
+//     console.log(num1 + num2);
+// }
+// add(5, 10);
+
+// function printValues(num, boolean, name, another){
+//     console.log(boonlean);
+// }
+// printValues(10, 'Allen', false, 'another');
+
+function printNames(names){
+    for(const name of names){
+        console.log(name);
+    }
+}
+printNames(['Bob', 'Jimmy', 'Julie'])
+
+const someStr = 'some string';
+console.log(someStr[6]);
+
+// const someName = 'Christian';
+// const h1 = document.querySelector('h1');
+
+h1.innerText =  someName;
